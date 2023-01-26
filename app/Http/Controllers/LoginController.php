bfcafe74110ef312899448c52147c7bb8c49c2c0
tiago-rodrigues1 <tiagords127@gmail.com>
2023-01-26 11:52:13 -0300

    add metodo de login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,24 +5,32 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Usuario;
 
-class LoginController extends Controller
-{
+class LoginController extends Controller {
     
-    public function login()
-    {
+    public function login() {
         return view('login');
     }
 
-    public function cadastrar(Request $request)
-    {
+    public function cadastrar(Request $request) {
         $usuario = Usuario::criarConta($request->nome,
-            $request->endereco, $request->senha,
-            $request->repetirSenha);
-        if ($usuario != null)
-        {
+        $request->endereco, $request->senha,
+        $request->repetirSenha);
+
+        if ($usuario != null) {
             return redirect('/');
         }
+
         return 'Deu erro!';
     }
 
+    public function logar(Request $request) {
+        $nome = $request->nome;
+        $senha = $request->senha;
+
+        if (Usuario::logar($nome, $senha)) {
+            return view('agenda');
+        } 
+
+        return redirect('/');
+    }
 }
